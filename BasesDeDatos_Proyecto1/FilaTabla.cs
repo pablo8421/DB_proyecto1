f89f86741cbc36e980f0c2ae95b2f332dd579a11
pablo8421 <pablo_jsd-8@hotmail.com>
2015-03-25 01:07:00 -0600

    Completada, ish, la tabla FilaTabla

--- BasesDeDatos_Proyecto1/FilaTabla.cs
+++ BasesDeDatos_Proyecto1/FilaTabla.cs
@@ -28,8 +28,69 @@ namespace BasesDeDatos_Proyecto1
         public bool cargar()
         {
             bool retorno = msgPack.LoadFileAsBytes(BDenUso + "\\" + tabla.nombre + ".data");
-            msgPack.DecodeFromBytes(unpack.GetAsBytes());
+            msgPack.DecodeFromBytes(msgPack.GetAsBytes());
             return retorno;
         }
+
+        public int getTamanio()
+        {
+            return tabla.cantidad_registros;
+        }
+
+        private MsgPack getRowMsqPack(int num)
+        {
+            return msgPack.ForcePathObject("row_"+num);
+        }
+
+        public List<Object> getRow(int num)
+        {
+            List<Object> lista = new List<Object>();
+            int i = 0;
+            foreach (MsgPack item in getRowMsqPack(num))
+            {
+                if(tabla.tipos_columnas[i].Equals("INT")){
+                    lista.Add(item.AsInteger);
+                }
+                if (tabla.tipos_columnas[i].Equals("FLOAT"))
+                {
+                    lista.Add(item.AsFloat);
+                }
+                if (tabla.tipos_columnas[i].Equals("DATE"))
+                {
+                    lista.Add(item.AsString);
+                }
+                if (tabla.tipos_columnas[i].StartsWith("CHAR"))
+                {
+                    lista.Add(item.AsString);
+                }
+                i++;
+            }
+            return lista;
+        }
+
+        public Object getRowElement(int row, int columna)
+        {
+            if (tabla.tipos_columnas[columna].Equals("INT"))
+            {
+                return msgPack.ForcePathObject("row_" + row).AsArray[columna].AsInteger;
+            }
+            else if (tabla.tipos_columnas[columna].Equals("FLOAT"))
+            {
+                return msgPack.ForcePathObject("row_" + row).AsArray[columna].AsFloat;
+            }
+            else if (tabla.tipos_columnas[columna].Equals("DATE"))
+            {
+                return msgPack.ForcePathObject("row_" + row).AsArray[columna].AsString;
+            }
+            else if (tabla.tipos_columnas[columna].StartsWith("CHAR"))
+            {
+                return msgPack.ForcePathObject("row_" + row).AsArray[columna].AsString;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
     }
 }
