5ccf9cd68fd58a6319d88bd7872181771b2e7d5a
pablo8421 <pablo_jsd-8@hotmail.com>
2015-04-08 01:19:13 -0600

    asdf

--- BasesDeDatos_Proyecto1/Form1.cs
+++ BasesDeDatos_Proyecto1/Form1.cs
@@ -24,6 +24,33 @@ namespace BasesDeDatos_Proyecto1
             databaseActual = "";
         }
 
+        private void dataGridView1_SortCompare(object sender, DataGridViewSortCompareEventArgs e)
+        {
+            try
+            {
+                if (DBNull.Value.Equals(e.CellValue1) || DBNull.Value.Equals(e.CellValue2))
+                {
+                    if (DBNull.Value.Equals(e.CellValue1) || e.CellValue1.Equals(null))
+                    {
+                        e.SortResult = 1;
+                    }
+                    else if (DBNull.Value.Equals(e.CellValue2) || e.CellValue2.Equals(null))
+                    {
+                        e.SortResult = -1;
+                    }
+                }
+                else
+                {
+                    e.SortResult = (e.CellValue1 as IComparable).CompareTo(e.CellValue2 as IComparable);
+                }
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
         private void boton_cargar_Click(object sender, EventArgs e)
         {
             OpenFileDialog choofdlog = new OpenFileDialog();
