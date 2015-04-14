5bb065eb5923d687abe3daf4a1919a29a05a1667
pablo8421 <pablo_jsd-8@hotmail.com>
2015-04-13 22:37:46 -0600

    f

--- BasesDeDatos_Proyecto1/TypeSystem.cs
+++ BasesDeDatos_Proyecto1/TypeSystem.cs
@@ -18,7 +18,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
 using System.Xml.Serialization;
-
+//COMENTARIO TONTO
 namespace BasesDeDatos_Proyecto1
 {
     class TypeSystem : SqlBaseVisitor<String>
